f3700d7a5ba701e5d3d7c713158a8408fd444d83
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-10-12 12:35:48 -0700

    updated app name

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Did you turn it off and turn it back on again?";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
