0290de07a8ba5be2d8190b4314b3daafd0879b0f
Jsanc189 <146498952+Jsanc189@users.noreply.github.com>
2024-10-14 17:45:20 -0700

    Step 2 complete:  Added event listeners and clear button

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,39 @@ canvas.height = 256;
 canvas.id = `canvas`;
 app.appendChild(canvas);
 
+
+const div = document.createElement('div');
+div.style.height = '20px';
+app.appendChild(div);
+
+const drawingContext = canvas.getContext('2d');
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener('mousedown', (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (cursor.active) {
+       drawingContext.beginPath();
+       drawingContext.moveTo(cursor.x, cursor.y);
+       drawingContext.lineTo(e.offsetX, e.offsetY);
+       drawingContext.stroke();
+       cursor.x = e.offsetX;
+       cursor.y = e.offsetY;
+     }
+});
+
+canvas.addEventListener('mouseup', () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement('button');
+clearButton.innerHTML = 'Clear';
+clearButton.addEventListener('click', () => {
+  drawingContext.clearRect(0, 0, canvas.width, canvas.height);
+});
+app.appendChild(clearButton);
